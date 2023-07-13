c2547684d83098417430d505b55fdbfdd78122da
JaceyB-TV <139434535+JaceyB-TV@users.noreply.github.com>
2023-07-13 09:59:43 +0100

    Removed weird spacing, removed interval

--- schedule.js
+++ schedule.js
@@ -1,59 +1,28 @@
 const dateOptions = {
-
     hour: "2-digit",
-
     minute: "2-digit",
-
     hour12: true
-
 };
 
-
-
 function updateSchedule() {
+    $(".table-container").empty()
 
-    $(".main-container").empty()
-
-
-
-    $("<div>").appendTo(".main-container").html("Schedule");
-
-
-
-    const table = $("<div>").addClass("table").appendTo(".main-container"),
-
+    const table = $("<div>").addClass("table").appendTo(".table-container"),
         now = new Date();
 
-
-
     schedule.forEach(item => {
-
         const row = $("<div>").addClass("row").appendTo(table),
-
             start = item.start.toLocaleDateString("en-GB", dateOptions).substr(12).toUpperCase();
 
-
-
         if (item.start < now && now < item.end) {
-
             row.addClass("glow-on-hover");
-
         }
 
-
-
         $("<div>").addClass("left cell").appendTo(row).html(start);
-
         $("<div>").addClass("right cell").appendTo(row).html(item.name);
-
     });
-
 }
 
-
-
-setInterval(updateSchedule, 5000);
-
-
+// setInterval(updateSchedule, 5000);
 
 updateSchedule();
