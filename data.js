99fd21b458cd29f16184d68c795e35b17151e7e9
JaceyB-TV <139434535+JaceyB-TV@users.noreply.github.com>
2023-07-13 08:47:15 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,93 @@
+const game = "Legend of Zelda: Tears of the Kingdom", schedule = [
+
+    {
+
+        name: "Welcome / Just Chatting",
+
+        start: new Date(2023, 6, 12, 19, 15, 0, 0),
+
+        end: new Date(2023, 6, 12, 19, 30, 0, 0)
+
+    },
+
+    {
+
+        name: "Duolingo",
+
+        start: new Date(2023, 6, 12, 19, 30, 0, 0),
+
+        end: new Date(2023, 6, 12, 20, 0, 0, 0)
+
+    },
+
+    {
+
+        name: "Stretch Break",
+
+        start: new Date(2023, 6, 12, 20, 0, 0, 0),
+
+        end: new Date(2023, 6, 12, 20, 2, 0, 0)
+
+    },
+
+    {
+
+        name: game,
+
+        start: new Date(2023, 6, 12, 20, 2, 0, 0),
+
+        end: new Date(2023, 6, 12, 21, 0, 0, 0)
+
+    },
+
+    {
+
+        name: "Stretch Break",
+
+        start: new Date(2023, 6, 12, 21, 0, 0, 0),
+
+        end: new Date(2023, 6, 12, 21, 2, 0, 0)
+
+    },
+
+    {
+
+        name: game,
+
+        start: new Date(2023, 6, 12, 21, 2, 0, 0),
+
+        end: new Date(2023, 6, 12, 22, 0, 0, 0)
+
+    },
+
+    {
+
+        name: "Stretch Break",
+
+        start: new Date(2023, 6, 12, 22, 0, 0, 0),
+
+        end: new Date(2023, 6, 12, 22, 2, 0, 0)
+
+    },
+
+    {
+
+        name: game,
+
+        start: new Date(2023, 6, 12, 22, 2, 0, 0),
+
+        end: new Date(2023, 6, 12, 22, 30, 0, 0)
+
+    },
+
+    {
+
+        name: "Goobey Forever",
+
+        start: new Date(2023, 6, 12, 22, 30, 0, 0),
+
+        end: new Date(2023, 6, 12, 23, 0, 0, 0)
+
+    }
+
+]
