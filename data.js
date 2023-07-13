22222453e25884853e82e7c9b226c9d106eb533c
JaceyB-TV <jaceyb.tv@gmail.com>
2023-07-13 23:08:16 +0100

    Fixed schedule layout Fixed glowing effect around current item Fixed Text centering when row is double height Added css variables for colours Updated schedule data to use current date Re-added 5 second refresh interval

--- data.js
+++ data.js
@@ -1,47 +1,47 @@
 const game = "Legend of Zelda: Tears of the Kingdom", schedule = [
     {
         name: "Welcome / Just Chatting",
-        start: new Date(2023, 6, 12, 19, 15, 0, 0),
-        end: new Date(2023, 6, 12, 19, 30, 0, 0)
+        start: new Date().setHours(19, 15, 0, 0),
+        end: new Date().setHours(19, 30, 0, 0)
     },
     {
         name: "Duolingo",
-        start: new Date(2023, 6, 12, 19, 30, 0, 0),
-        end: new Date(2023, 6, 12, 20, 0, 0, 0)
+        start: new Date().setHours(19, 30, 0, 0),
+        end: new Date().setHours(20, 0, 0, 0)
     },
     {
         name: "Stretch Break",
-        start: new Date(2023, 6, 12, 20, 0, 0, 0),
-        end: new Date(2023, 6, 12, 20, 2, 0, 0)
+        start: new Date().setHours(20, 0, 0, 0),
+        end: new Date().setHours(20, 2, 0, 0)
     },
     {
         name: game,
-        start: new Date(2023, 6, 12, 20, 2, 0, 0),
-        end: new Date(2023, 6, 12, 21, 0, 0, 0)
+        start: new Date().setHours(20, 2, 0, 0),
+        end: new Date().setHours(21, 0, 0, 0)
     },
     {
         name: "Stretch Break",
-        start: new Date(2023, 6, 12, 21, 0, 0, 0),
-        end: new Date(2023, 6, 12, 21, 2, 0, 0)
+        start: new Date().setHours(21, 0, 0, 0),
+        end: new Date().setHours(21, 2, 0, 0)
     },
     {
         name: game,
-        start: new Date(2023, 6, 12, 21, 2, 0, 0),
-        end: new Date(2023, 6, 12, 22, 0, 0, 0)
+        start: new Date().setHours(21, 2, 0, 0),
+        end: new Date().setHours(22, 0, 0, 0)
     },
     {
         name: "Stretch Break",
-        start: new Date(2023, 6, 12, 22, 0, 0, 0),
-        end: new Date(2023, 6, 12, 22, 2, 0, 0)
+        start: new Date().setHours(22, 0, 0, 0),
+        end: new Date().setHours(22, 2, 0, 0)
     },
     {
         name: game,
-        start: new Date(2023, 6, 12, 22, 2, 0, 0),
-        end: new Date(2023, 6, 12, 22, 30, 0, 0)
+        start: new Date().setHours(22, 2, 0, 0),
+        end: new Date().setHours(22, 30, 0, 0)
     },
     {
         name: "Goobey Forever",
-        start: new Date(2023, 6, 12, 22, 30, 0, 0),
-        end: new Date(2023, 6, 15, 23, 0, 0, 0)
+        start: new Date().setHours(22, 30, 0, 0),
+        end: new Date().setHours(23, 0, 0, 0)
     }
 ]
--- schedule.js
+++ schedule.js
@@ -8,21 +8,24 @@ function updateSchedule() {
     $(".table-container").empty()
 
     const table = $("<div>").addClass("table").appendTo(".table-container"),
-        now = new Date();
+        now = Date.now();
 
     schedule.forEach(item => {
         const row = $("<div>").addClass("row").appendTo(table),
-            start = item.start.toLocaleDateString("en-GB", dateOptions).substr(12).toUpperCase();
+            start = new Date(item.start).toLocaleDateString("en-GB", dateOptions).substr(12).toUpperCase();
+
+        const left = $("<div>").addClass("left cell").appendTo(row).html(start);
 
         if (item.start < now && now < item.end) {
             row.addClass("glow-on-hover");
         }
 
-        $("<div>").addClass("left cell").appendTo(row).html(start);
+        //$("<div>").appendTo(left).html(start);
+
         $("<div>").addClass("right cell").appendTo(row).html(item.name);
     });
 }
 
-// setInterval(updateSchedule, 5000);
+ setInterval(updateSchedule, 5000);
 
 updateSchedule();
