fd6996fe82d909adb1d3c2c2bebba94b4b8df912
JaceyB-TV <139434535+JaceyB-TV@users.noreply.github.com>
2023-07-14 11:02:26 +0100

    Added debug mode that resets the times to hourly intervals and disables the auto update

--- schedule.js
+++ schedule.js
@@ -4,15 +4,26 @@ const dateOptions = {
     hour12: true
 };
 
+const debugMode = true;
+
 function updateSchedule() {
     $(".table-container").empty()
 
     const table = $("<div>").addClass("table").appendTo(".table-container"),
         now = Date.now();
 
+    if (debugMode) {
+        let start = 9;
+
+        schedule.forEach(item => {
+            item.start = new Date().setHours(start, 0, 0, 0);
+            item.end = new Date().setHours(++start, 0, 0, 0);
+        });
+    }
+
     schedule.forEach(item => {
         const row = $("<div>").addClass("row").appendTo(table),
-            start = new Date(item.start).toLocaleDateString("en-GB", dateOptions).substr(12).toUpperCase();
+            start = new Date(item.start).toLocaleDateString("en-GB", dateOptions).substring(12).toUpperCase();
 
         const left = $("<div>").addClass("left cell").appendTo(row).html(start);
 
@@ -26,6 +37,8 @@ function updateSchedule() {
     });
 }
 
- setInterval(updateSchedule, 5000);
+if (!debugMode) {
+    setInterval(updateSchedule, 5000);
+}
 
 updateSchedule();
